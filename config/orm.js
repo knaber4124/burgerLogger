eb329368c90cb4d4c565e187891a17c52d88be0e
Keith Naber <knaber4124@gmail.com>
2019-10-03 18:55:28 -0400

    updated orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,31 @@
 let connection = require('./connection');
 
 let orm = {
+    selectAll: function (table, callBack) {
+        let queryString = "SELECT * FROM burgers;";
+        connection.query(queryString, function (err, res) {
+            if (err) throw err;
+            callBack(res)
+        });
+    },
+    insertOne: function (table, cols, vals, callBack) {
+        let queryString = "INSERT INTO burgers burgerName VALUES ?;";
+        connection.query(queryString, val, function (err, res) {
+            if (err) throw err;
+            callBack(res);
+        })
+    },
+    updateOne: function (table, objColVal, condition, callBack) {
+        let queryString = "UPDATE burgers SET";
+        queryString += objToSql.objColVal;
+        queryString += "WHERE";
+        queryString += condition;
 
+        connection.query(queryString, function(err, res){
+            if (err) throw err;
+            callBack(res);
+        })
+    }
 }
 
 
