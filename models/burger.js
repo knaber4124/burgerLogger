de6e1700befbb46af1d38eac9c665fe60f61e630
Keith Naber <knaber4124@gmail.com>
2019-10-03 19:12:23 -0400

    updated burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,24 @@
 let orm = require('../config/orm');
 
+let burger = {
+    all: function(cb){
+        orm.selectAll("burgers",function(res){
+            cb(res);
+        })
+    },
+    create: function(col,val,cb){
+        orm.insertOne("burgers", col, val, function(res){
+            cb(res);
+        })
+    },
+    update: function(objColVal,condition,cb){
+        orm.updateOne("burgers", objColVal,condition, function(res){
+            cb(res);
+        })
+    }
+
+}
+
 
 
 
