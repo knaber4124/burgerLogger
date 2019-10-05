06cc76a5fcdfe6523ecfca9695f78c1b14067b1d
Keith Naber <knaber4124@gmail.com>
2019-10-05 14:59:38 -0400

    routes started

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,6 +1,39 @@
 let express = require('express');
 let burger = require('../models/burger');
 
+let router = express.Router();
+
+router.get('/', function(req,res){
+    burger.all(function(data){
+        let object = {
+            burger:data
+        };
+    })
+    console.log(object);
+    res.render('index',object);
+});
+
+router.post('/api/burgers', function(req,res){
+    burger.create(['name'],[req.body.name]), function(result){
+        res.json({id:result.insertId});
+    };
+});
+router.put('/api/burgers/:id', function(req, res){
+    let condition = "id = "+ req.params.id;
+    burger.update({
+        devoured: req.body.devoured
+    },
+    condition,
+    function(result){
+        if (result.changedRows === 0){
+            throw err;
+        } 
+        res.status(200).end();
+
+    })
+})
+
+
 
 
 
